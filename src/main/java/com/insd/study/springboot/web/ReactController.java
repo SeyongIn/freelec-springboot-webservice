4c9fce545a09ae640422f240325c2174c47e306e
thomas <insd77@naver.com>
2020-05-24 14:07:05 +0900

    React Test #1-1  - ReactController 반환 값 변경

--- src/main/java/com/insd/study/springboot/web/ReactController.java
+++ src/main/java/com/insd/study/springboot/web/ReactController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ReactController {
     @GetMapping("/react")
     public String postsSave() {
-        return "react";
+        return "react.html";
     }
 }
