f704d1e1cdba3f11cc9a7144c811bffedff36463
thomas <insd77@naver.com>
2020-05-24 13:54:38 +0900

    React Test #1

---
+++ src/main/java/com/insd/study/springboot/web/ReactController.java
@@ -0,0 +1,14 @@
+package com.insd.study.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class ReactController {
+    @GetMapping("/react")
+    public String postsSave() {
+        return "react";
+    }
+}
